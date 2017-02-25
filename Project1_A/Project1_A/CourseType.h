af753fca2a57f0b6ca8de854b4a4b45e2bfed22b
Barry P <Barry P@BARRY>
2017-02-24 20:30:25 -0800

    Finish up printCourse()

--- Project1_A/Project1_A/CourseType.h
+++ Project1_A/Project1_A/CourseType.h
@@ -2,6 +2,8 @@
    Viridian Dynamics
    Josh Downs
    Richie Stanley
+   Phan, Barry
+
 
    CS 250
    27 Feb 2017
@@ -23,25 +25,25 @@ const string PREFIX = "CS A";
 class CourseType
 {
 public:
-CourseType();
-CourseType(const string& newCourseName,
-											int newCourseNum, double newUnits);
+	CourseType();
+	CourseType(const string& newCourseName,
+		int newCourseNum, double newUnits);
 
-string getCourseName() const;
-int getCourseNumber() const;
-double getCourseUnits() const;
-string getPrefix() const;
+	string getCourseName() const;
+	int getCourseNumber() const;
+	double getCourseUnits() const;
+	string getPrefix() const;
 
-void setCourseName(const string& newName);
-void setCourseNumber(int newNum);
-void setCourseUnits(double newUnits);
+	void setCourseName(const string& newName);
+	void setCourseNumber(int newNum);
+	void setCourseUnits(double newUnits);
 
-void printCourse();
+	void printCourse();
 
 private:
-string courseName;
-int courseNum;
-double units;
+	string courseName;
+	int courseNum;
+	double units;
 };
 
 
